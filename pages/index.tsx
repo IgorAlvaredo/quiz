5817547a8681bd0a516e1fcd5e258ba6e7423f60
IgorAlvaredo <94122674+IgorAlvaredo@users.noreply.github.com>
2023-08-19 22:03:02 -0300

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,26 @@ import styles from '@/styles/Home.module.css'
 import Questao from '@/components/Questao'
 import QuestaoModel from '@/model/questao'
 import RespostaModel from '@/model/resposta'
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
+
+const questaoMock = new QuestaoModel(1, 'Melhor cor?', [
+  RespostaModel.errada('Verde'),
+  RespostaModel.errada('Vermelha'),
+  RespostaModel.errada('Azul'),
+  RespostaModel.certa('preta')
+])
+
 export default function Home() {
-  const questaoTeste = new QuestaoModel(1, 'Melhor cor?', [
-    RespostaModel.errada('Verde'),
-    RespostaModel.errada('Vermelha'),
-    RespostaModel.errada('Azul'),
-    RespostaModel.certa('preta')
-  ])
+  const [questao, setQuestao] = useState(questaoMock)
+
+  function respostaFornecida(indice: number) {
+    console.log(indice)
+    setQuestao(questao.responderCom(indice))
+  }
+
   return (
     <div style={{
       display: 'flex',
@@ -22,7 +32,8 @@ export default function Home() {
       alignItems: 'center',
       height: '100vh'
     }}>
-      <Questao valor={questaoTeste}/>
+      <Questao valor={questao}
+        respostaFornecida={respostaFornecida} />
     </div>
   )
 }
