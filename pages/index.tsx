f8b00de1571cc99f240f5934d20f46981e9633fd
IgorAlvaredo <94122674+IgorAlvaredo@users.noreply.github.com>
2023-08-21 22:46:47 -0300

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import Botao from '@/components/Botao'
 import Questao from '@/components/Questao'
+import Questionario from '@/components/Questionario'
 import QuestaoModel from '@/model/questao'
 import RespostaModel from '@/model/resposta'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const questaoMock = new QuestaoModel(1, 'Qual é a melhor cor?', [
   RespostaModel.errada('Verde'),
@@ -11,33 +12,46 @@ const questaoMock = new QuestaoModel(1, 'Qual é a melhor cor?', [
   RespostaModel.certa('Preta')
 ])
 
-export default function Home() {
-  const [questao, setQuestao] = useState(questaoMock)
+const BASE_URL = 'http://localhost:3000/api'
 
-  function respostaFornecida(indice: number) {
-    console.log(indice)
-    setQuestao(questao.responderCom(indice))
+export default function Home() {
+  const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([])
+  const [questao, setQuestao] = useState(questaoMock)
+  
+  async function carregarIdsDasQuestoes() {
+    const resp = await fetch(`${BASE_URL}/questionario`)
+    const idsDasQuestoes = await resp.json()
+    setIdsDasQuestoes(idsDasQuestoes)
   }
 
-  function tempoEsgotado() {
-    if(questao.naoRespondida){  
-      setQuestao(questao.responderCom(-1))
-    }
+  async function carregarQuestao(idQuestao: number) {
+    const resp = await fetch(`${BASE_URL}/questoes/${idQuestao}`)
+    const json = await resp.json()
+  }
+  
+  useEffect(() => {
+    carregarIdsDasQuestoes()
+  }, [])
+
+  useEffect(() => {
+    idsDasQuestoes.length > 0 && carregarQuestao(idsDasQuestoes[0])
+    carregarIdsDasQuestoes()
+  }, [idsDasQuestoes])
+
+  function questaoRespondida(questao: QuestaoModel) {
+
+  }
+
+  function irPraProximoPasso() {
+
   }
 
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100vh'
-    }}>
-      <Questao valor={questao}
-        respostaFornecida={respostaFornecida} 
-        tempoEsgotado={tempoEsgotado}/>
-        
-      <Botao texto="Próxima" href='/resultado'/>
-    </div>
+    <Questionario
+      questao={questao}
+      ultima={true}
+      questaoRespondida={questaoRespondida}
+      irPraProximoPasso={irPraProximoPasso}
+    />
   )
 }
