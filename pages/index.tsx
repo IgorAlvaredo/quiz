890ca8fcbc6b9b69c923e7bc87529b2c5e0ec67e
IgorAlvaredo <94122674+IgorAlvaredo@users.noreply.github.com>
2023-08-23 23:41:45 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import QuestaoModel from '@/model/questao'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 
-const BASE_URL = 'https://quiz-henna-beta.vercel.app/api'
+const BASE_URL = 'https://quiz-tau-bay.vercel.app/api'
 
 export default function Home() {
   const router = useRouter()
